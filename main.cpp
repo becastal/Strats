7339a5d3fdc1a26992932b3bd3598feb7036dc36
Bernardo Castaldelli <bernardo.castaldelli@gmail.com>
2026-04-30 11:13:06 -0300

    logica pra peg ok

--- main.cpp
+++ main.cpp
@@ -34,44 +34,106 @@ public:
 		resolveFilaVendas();
 	}
 
+	void adicionaPegBidBuy(int id, int quantidade) {
+		ordensPegCompra[id] = quantidade;
+
+		resolveFilaVendas();
+		resolveOrdens();
+	}
+
+	void adicionaPegOfferSell(int id, int quantidade) {
+		ordensPegVenda[id] = quantidade;
+
+		resolveFilaCompras();
+		resolveOrdens();
+	}
+
 	void imprimeLivro() {
 		int W = 40;
+		auto it_compra = ordensCompras.begin();
+		auto it_venda = ordensVendas.begin();
+		auto it_peg_compra = ordensPegCompra.begin();
+		auto it_peg_venda = ordensPegVenda.begin();
+		bool temPegCompra = not ordensCompras.empty();
+		bool temPegVenda = not ordensVendas.empty();
+		double precoPegCompra = (temPegCompra ? -(*ordensCompras.begin()).first.first : 0);
+		double precoPegVenda = (temPegVenda ? (*ordensVendas.begin()).first.first : 0);
 
 		cout << "+" << string(W + 2, '-') << "+" << string(W + 2, '-') << "+\n";
 		cout << "| " << left << setw(W) << "Ordens de Compra" << " | " << left << setw(W) << "Ordens de Venda" << " |\n"; 
 		cout << "+" << string(W + 2, '-') << "+" << string(W + 2, '-') << "+\n";
 
-		auto it_compra = ordensCompras.begin();
-		auto it_venda = ordensVendas.begin();
-
-		while (it_compra != ordensCompras.end() or it_venda != ordensVendas.end()) {
+		while (it_compra != ordensCompras.end() or it_venda != ordensVendas.end() or
+			   (temPegCompra and it_peg_compra != ordensPegCompra.end()) or
+			   (temPegVenda and it_peg_venda != ordensPegVenda.end())) {
 			cout << "| ";
 
-			if (it_compra != ordensCompras.end()) {
-				int quantidade = (*it_compra).second;
-				double preco = -(*it_compra).first.first;
+			if (it_compra != ordensCompras.end() or (temPegCompra and it_peg_compra != ordensPegCompra.end())) {
+				bool usaPeg = false;
+
+				if (temPegCompra and it_peg_compra != ordensPegCompra.end()) {
+					if (it_compra == ordensCompras.end()) {
+						usaPeg = true;
+					} else {
+						double preco = -(*it_compra).first.first;
+						int id = (*it_compra).first.second;
+						usaPeg = (preco < precoPegCompra or (preco == precoPegCompra and (*it_peg_compra).first < id));
+					}
+				}
+
+				int quantidade;
+				double preco;
+				if (usaPeg) {
+					quantidade = (*it_peg_compra).second;
+					preco = precoPegCompra;
+					it_peg_compra = next(it_peg_compra);
+				} else {
+					quantidade = (*it_compra).second;
+					preco = -(*it_compra).first.first;
+					it_compra = next(it_compra);
+				}
 
 				stringstream ss;
 				ss << quantidade << " @ " << preco;
+				if (usaPeg) ss << " (pegged)";
 
 				cout << left << setw(W) << ss.str();
-				it_compra = next(it_compra);
 			} else {
 				cout << left << setw(W) << "";
 			}
 
 			cout << " | ";
 
-			if (it_venda != ordensVendas.end()) {
-				int quantidade = (*it_venda).second;
-				double preco = (*it_venda).first.first;
+			if (it_venda != ordensVendas.end() or (temPegVenda and it_peg_venda != ordensPegVenda.end())) {
+				bool usaPeg = false;
+
+				if (temPegVenda and it_peg_venda != ordensPegVenda.end()) {
+					if (it_venda == ordensVendas.end()) {
+						usaPeg = true;
+					} else {
+						double preco = (*it_venda).first.first;
+						int id = (*it_venda).first.second;
+						usaPeg = (preco > precoPegVenda or (preco == precoPegVenda and (*it_peg_venda).first < id));
+					}
+				}
+
+				int quantidade;
+				double preco;
+				if (usaPeg) {
+					quantidade = (*it_peg_venda).second;
+					preco = precoPegVenda;
+					it_peg_venda = next(it_peg_venda);
+				} else {
+					quantidade = (*it_venda).second;
+					preco = (*it_venda).first.first;
+					it_venda = next(it_venda);
+				}
 
 				stringstream ss;
 				ss << quantidade << " @ " << fixed << setprecision(2) << preco;
+				if (usaPeg) ss << " (pegged)";
 
 				cout << left << setw(W) << ss.str();
-
-				it_venda = next(it_venda);
 			} else {
 				cout << left << setw(W) << "";
 			}
@@ -84,7 +146,7 @@ public:
 	void cancelaOrdem(int id) {
 		auto it_compra = indiceCompras.find(id);
 		if (it_compra != indiceCompras.end()) {
-			removeCompra((*it_conot mpra).second);
+			removeCompra((*it_compra).second);
 			return;
 		}
 
@@ -96,7 +158,11 @@ public:
 
 		if (quantidadeFila.count(id)) {
 			quantidadeFila[id] = 0;
+			return;
 		}
+
+		if (ordensPegCompra.count(id)) ordensPegCompra.erase(id);
+		if (ordensPegVenda.count(id)) ordensPegVenda.erase(id);
 	}
 
 	void alteraOrdemMarket(int id, int novaQuantidade) {
@@ -121,6 +187,7 @@ private:
 	map<pair<double, int>, int> ordensCompras, ordensVendas; // [[preco, id], quantidade]
 	map<int, map<pair<double, int>, int>::iterator> indiceCompras, indiceVendas; // [id, iterador pra ordens*]
 	map<int, int> quantidadeFila; // [id, quantidade da ordem market]
+	map<int, int> ordensPegCompra, ordensPegVenda; // [id, quantidade]
 
 	void notificaTrade(double preco, int quantidade) {
 		cout << "Trade, price: " << preco << ", qty: " << quantidade << "\n";
@@ -130,17 +197,36 @@ private:
 		while (not ordensVendas.empty() and not ordensCompras.empty()) {
 			auto it_compra = ordensCompras.begin();
 			auto it_venda = ordensVendas.begin();
+			auto it_peg_compra = ordensPegCompra.begin();
+			auto it_peg_venda = ordensPegVenda.begin();
 
-			if (-(*it_compra).first.first < (*it_venda).first.first) break;
+			double precoCompra = -(*it_compra).first.first;
+			double precoVenda = (*it_venda).first.first;
 
-			int tira = min((*it_compra).second, (*it_venda).second);
+			if (precoCompra < precoVenda) break;
 
-			notificaTrade(-(*it_compra).first.first, tira);
+			bool usaPegCompra = (it_peg_compra != ordensPegCompra.end() and (*it_peg_compra).first < (*it_compra).first.second);
+			bool usaPegVenda = (it_peg_venda != ordensPegVenda.end() and (*it_peg_venda).first < (*it_venda).first.second);
 
-			if (((*it_compra).second -= tira) == 0) { 
+			int quantidadeCompra = (usaPegCompra ? (*it_peg_compra).second : (*it_compra).second);
+			int quantidadeVenda = (usaPegVenda ? (*it_peg_venda).second : (*it_venda).second);
+			int tira = min(quantidadeCompra, quantidadeVenda);
+
+			notificaTrade(precoCompra, tira);
+
+			if (usaPegCompra) {
+				if (((*it_peg_compra).second -= tira) == 0) {
+					ordensPegCompra.erase(it_peg_compra);
+				}
+			} else if (((*it_compra).second -= tira) == 0) { 
 				removeCompra(it_compra);
 			}
-			if (((*it_venda).second -= tira) == 0) { 
+
+			if (usaPegVenda) {
+				if (((*it_peg_venda).second -= tira) == 0) {
+					ordensPegVenda.erase(it_peg_venda);
+				}
+			} else if (((*it_venda).second -= tira) == 0) { 
 				removeVenda(it_venda);
 			}
 		}
@@ -152,13 +238,21 @@ private:
 			
 			int id = filaCompras.front();
 			int& quant = quantidadeFila[id];
-			for (auto it = ordensVendas.begin(); it != ordensVendas.end() and quant > 0; ) {
-				int tira = min(quant, (*it).second);
+			while (not ordensVendas.empty() and quant > 0) {
+				auto it_venda = ordensVendas.begin();
+				auto it_peg_venda = ordensPegVenda.begin();
+				bool usaPegVenda = (it_peg_venda != ordensPegVenda.end() and (*it_peg_venda).first < (*it_venda).first.second);
+				int quantidadeVenda = (usaPegVenda ? (*it_peg_venda).second : (*it_venda).second);
+				int tira = min(quant, quantidadeVenda);
 
-				notificaTrade((*it).first.first, tira);
+				notificaTrade((*it_venda).first.first, tira);
 
-				if (((*it).second -= tira) == 0) {
-					it = removeVenda(it);
+				if (usaPegVenda) {
+					if (((*it_peg_venda).second -= tira) == 0) {
+						ordensPegVenda.erase(it_peg_venda);
+					}
+				} else if (((*it_venda).second -= tira) == 0) {
+					removeVenda(it_venda);
 				}
 				if ((quant -= tira) == 0) {
 					break;
@@ -178,13 +272,21 @@ private:
 			
 			int id = filaVendas.front();
 			int& quant = quantidadeFila[id];
-			for (auto it = ordensCompras.begin(); it != ordensCompras.end() and quant > 0; ) {
-				int tira = min(quant, (*it).second);
+			while (not ordensCompras.empty() and quant > 0) {
+				auto it_compra = ordensCompras.begin();
+				auto it_peg_compra = ordensPegCompra.begin();
+				bool usaPegCompra = (it_peg_compra != ordensPegCompra.end() and (*it_peg_compra).first < (*it_compra).first.second);
+				int quantidadeCompra = (usaPegCompra ? (*it_peg_compra).second : (*it_compra).second);
+				int tira = min(quant, quantidadeCompra);
 
-				notificaTrade(-(*it).first.first, tira);
+				notificaTrade(-(*it_compra).first.first, tira);
 
-				if (((*it).second -= tira) == 0) {
-					it = removeCompra(it);
+				if (usaPegCompra) {
+					if (((*it_peg_compra).second -= tira) == 0) {
+						ordensPegCompra.erase(it_peg_compra);
+					}
+				} else if (((*it_compra).second -= tira) == 0) {
+					removeCompra(it_compra);
 				}
 				if ((quant -= tira) == 0) {
 					break;
@@ -246,6 +348,11 @@ int main() {
 			}
 		} else if (tipo == "limit") {
 			string lado; cin >> lado;
+
+			if (lado == "bid" or lado == "offer") {
+				cin >> lado;
+			}
+
 			double preco; cin >> preco;
 			int quantidade; cin >> quantidade;
 
@@ -255,6 +362,21 @@ int main() {
 				gestor.adicionaLimitBuy(id, quantidade, preco);
 			} else if (lado == "sell") {
 				gestor.adicionaLimitSell(id, quantidade, preco);
+			} else assert(0);
+		} else if (tipo == "peg") {
+			string referencia, lado; cin >> referencia >> lado;
+			int quantidade; cin >> quantidade;
+
+			cout << ">>> new peg " << referencia << " " << lado << " order qty: " << quantidade << '\n';
+
+			if (referencia == "offer" and lado == "buy") {
+				gestor.adicionaMarketBuy(id, quantidade);
+			} else if (referencia == "bid" and lado == "sell") {
+				gestor.adicionaMarketSell(id, quantidade);
+			} else if (referencia == "bid" and lado == "buy") {
+				gestor.adicionaPegBidBuy(id, quantidade);
+			} else if (referencia == "offer" and lado == "sell") {
+				gestor.adicionaPegOfferSell(id, quantidade);
 			} else assert(0);
 		} else if (tipo == "market") {
 			string lado; cin >> lado;
